532cd1fb9dea395fd1b1adcf4d026c32fe82281c
willow <shaneweth@gmail.com>
2019-04-03 14:02:44 -0400

    viable product. CSS, HTML updated and JS is functional. Need to account for daylight savings and see if there is any update to the JS function.

---
+++ assets/js/logic.js
@@ -0,0 +1,25 @@
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand  = document.querySelector('.hour-hand')
+
+function setDate() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDeg = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDeg}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDeg = ((mins / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minsDeg}deg)`
+
+    const hours = now.getHours();
+    const hoursDeg = ((hours / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${hoursDeg}deg)`
+
+
+
+    console.log(seconds, mins, hours);
+}
+
+setInterval(setDate, 1000); 
